e841e6371913617fdb8c4b87010d3035bd6be59e
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2014-07-10 12:43:06 +0300

    update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,11 +23,11 @@ set :deploy_to, '/home/deploy/food_search'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{log tmp}
+set :linked_dirs, %w{log tmp public/assets assets_manifest_backup}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -36,6 +36,7 @@ set :linked_dirs, %w{log tmp}
 # set :keep_releases, 5
 
 set :rvm_ruby_version, '2.0.0@food_search'
+set :rvm_custom_path, '/usr/local/rvm'
 
 namespace :deploy do
 
@@ -43,7 +44,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
@@ -52,9 +53,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :rake, 'tmp:cache:clear'
+      end
     end
   end
 
